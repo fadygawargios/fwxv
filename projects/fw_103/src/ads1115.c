2873e39684cc2b36219820a54433ebc4c28bc19d
Fady Gawargios <fadyg06@gmail.com>
2024-11-02 14:26:59 -0400

    Done fw103

--- projects/fw_103/src/ads1115.c
+++ projects/fw_103/src/ads1115.c
@@ -15,18 +15,20 @@ StatusCode ads1115_init(ADS1115_Config *config, ADS1115_Address i2c_addr, GpioAd
 
   // Write Config register
   /* TODO: fill out this value */
-  cmd = 0x0000;
+  cmd = 0x0483;
+  // 0000 0100 1000 0011
+  
   i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_CONFIG, (uint8_t *)(&cmd), 2);
 
-  /* TODO (optional) */
+  // /* TODO (optional) */
   // Set low thresh to zero
-  cmd = 0x0000;
-  i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_LO_THRESH, (uint8_t *)(&cmd), 2);
+  // cmd = 0x0000;
+  // i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_LO_THRESH, (uint8_t *)(&cmd), 2);
 
-  /* TODO (optional) */
-  // Set high thresh to 1V
-  cmd = 0x0000;
-  i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_LO_THRESH, (uint8_t *)(&cmd), 2);
+  // /* TODO (optional) */
+  /// Set high thresh to 1V
+  // cmd = 0x0000;
+  // i2c_write_reg(config->i2c_port, i2c_addr, ADS1115_REG_LO_THRESH, (uint8_t *)(&cmd), 2);
 
   // Register the ALRT pin
   /* TODO (optional) */
@@ -49,10 +51,19 @@ StatusCode ads1115_select_channel(ADS1115_Config *config, ADS1115_Channel channe
 
 StatusCode ads1115_read_raw(ADS1115_Config *config, ADS1115_Channel channel, int16_t *reading) {
   /* TODO: complete function */
+  i2c_read_reg(config->i2c_port, config->i2c_addr, ADS1115_REG_CONVERSION, (uint8_t *) reading, 2);
   return STATUS_CODE_OK;
 }
 
 StatusCode ads1115_read_converted(ADS1115_Config *config, ADS1115_Channel channel, float *reading) {
   /* TODO: complete function */
+  const float MAX_VALUE = 65535.0;
+  const float MAX_VOLTAGE =  4.096;
+  int16_t whatever;
+
+  ads1115_read_raw(config, channel, &whatever);
+  
+
+  *(reading) = ((whatever)/MAX_VALUE)*(MAX_VOLTAGE);
   return STATUS_CODE_OK;
 }
--- projects/fw_103/src/main.c
+++ projects/fw_103/src/main.c
@@ -13,15 +13,57 @@
 
 #include "log.h"
 #include "tasks.h"
+#include "gpio.h"
+#include "delay.h"
+#include "ads1115.h"
+
+TASK(leds, TASK_STACK_512) {
+
+  float *reading;
+
+  GpioAddress led_addr = {
+  .port = GPIO_PORT_B,
+  .pin = 3,
+};
+
+  StatusCode gpio_init_pin(const GpioAddress *led_addr, const GpioMode pin_mode, GpioState init_state);
+
+    GpioAddress ready_pin = {
+    .port = GPIO_PORT_B,
+    .pin = GPIO_Pin_0,
+  };
+
+  ADS1115_Config config = {
+    .handler_task = leds,
+    .i2c_addr = ADS1115_ADDR_GND,
+    .i2c_port = ADS1115_I2C_PORT,
+    .ready_pin = &ready_pin,
+  };
+
+  float idk;
+  
+  while(true) {
+    gpio_toggle_state(&led_addr);
+
+    ads1115_read_converted(&config, ADS1115_CHANNEL_0, &idk);
+    LOG_DEBUG("%f\n", idk);
+    delay_ms(100);
+  }
+
+}
+
 
 int main() {
-  tasks_init();
-  log_init();
-  gpio_init();
+  StatusCode gpio_init();
+
+  log_init()
   LOG_DEBUG("Welcome to FW 103!\n");
 
+  tasks_init();
+  tasks_init_task(leds, TASK_PRIORITY(1), NULL); 
   tasks_start();
 
-  LOG_DEBUG("exiting main?\n");
+
+  // LOG_DEBUG("exiting main?\n");
   return 0;
 }
